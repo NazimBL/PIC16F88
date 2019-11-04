93fa3d7fe84714c354a09749a74b44cf1a3dbfb4
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2019-11-04 10:38:56 +0100

    Update Assembly.asm

--- Assembly.asm
+++ Assembly.asm
@@ -1,15 +1,18 @@
+ */
+ * Nazim BL
+ * MPLAB X IDE
+ */ 
+ 
  #include <p16f88.inc>
+ 
  __CONFIG _CONFIG1, _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _BOREN_ON & _LVP_ON & _CPD_OFF & _WRT_OFF & _CCPMX_RB0 & _CP_OFF
  __CONFIG _CONFIG2, _FCMEN_ON & _IESO_ON
     
- org 0
-
- 
+ org 0 
  Main:
    ;select bank , trisb = bank 1 , place =86h
    bsf STATUS,RP0
-   bcf STATUS,RP1
-   
+   bcf STATUS,RP1   
      
    clrw
    movwf TRISB
@@ -26,5 +29,4 @@
     movwf PORTB
    
    goto Loop
-    
- end
+   end
